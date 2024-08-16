7d3d2c44ba6b099897d59d815ac0bf176aa76fdc
Gena <thipi1213@live.com>
2024-08-16 18:59:04 -0400

    Trying method from developers.google

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
 import './App.css'
-import React, {useEffect, useState } from 'react'
-import {gapi} from 'gapi-script'
-import EventItem from "./components/EventItem.js"
 
 function App() {
+  const {google} = require('googleapis');
+  const crypto = require('crypto');
+  const express = require('express');
+  const session = require('express-session');
+
+  /**
+   * To use OAuth2 authentication, we need access to a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI
+   * from the client_secret.json file. To get these credentials for your application, visit
+   * https://console.cloud.google.com/apis/credentials.
+   */
   
-
-  const calendarID= process.env.CALENDAR_ID
-  const apiKey = process.env.GOOGLE_API_KEY
-  const accessToken = process.env.GOOGLE_ACCESS_TOKEN
-
-  const getGoogleCalendarEvents = (calendarID, apiKey) => {
-    function initiate() {
-        gapi.client
-            .init({
-                apiKey: apiKey,
-            })
-
-            .then(function () {
-                return gapi.client.request({
-                    path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
-                })
-            })
-
-            .then(
-                (response) => {
-                    let calendar_events = response.result.items
-                    return calendar_events
-                },
-                function (err) {
-                    return [false, err]
-                }
-            )
-    }
-
-    gapi.load('client', initiate)
-  }
-
-
-  return (
-    <div className="App">
-      <h1>Coordinator app</h1>
-    </div>
+  const oauth2Client = new google.auth.OAuth2(
+    '475553630933-f5mu6cflagmam5s3isbho3n3apii9ka3.apps.googleusercontent.com',
+    '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',
+    'http://localhost:8080'
   );
+
+  // Access scopes for read-only Drive activity.
+  const scopes = [
+    'https://www.googleapis.com/auth/drive.metadata.readonly'
+  ];
+
+  // Generate a secure random state value.
+  const state = crypto.randomBytes(32).toString('hex');
+
+  // Store state in the session
+  req.session.state = state;
+
+  // Generate a url that asks permissions for the Drive activity scope
+  const authorizationUrl = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'offline',
+    /** Pass in the scopes array defined above.
+      * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
+    scope: scopes,
+    // Enable incremental authorization. Recommended as a best practice.
+    include_granted_scopes: true,
+    // Include the state parameter to reduce the risk of CSRF attacks.
+    state: state
+  });
+
 }
 
 export default App;
---
+++ src/components/EventItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './EventItem.css'
+
+export default function EventItem({description}) {
+    return (
+        <div>
+            <p>{description}</p>
+        </div>
+
+    )
+} 
