e0888116e6718f9d008ce8700b21f9cf4b63197e
Gena <thipi1213@live.com>
2024-08-30 16:50:28 -0400

    Pushing before making changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,102 +1,6 @@
-// // Generated using webpack-cli https://github.com/webpack/webpack-cli
+const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-
-// const isProduction = process.env.NODE_ENV == 'production';
-
-
-// const stylesHandler = MiniCssExtractPlugin.loader;
-
-
-
-// const config = {
-//     entry: './src/index.js',
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//     },
-//     devServer: {
-//         open: true,
-//         host: 'localhost',
-//     },
-//     plugins: [
-//         new HtmlWebpackPlugin({
-//             template: 'index.html',
-//         }),
-
-//         new MiniCssExtractPlugin(),
-
-//         // Add your plugins here
-//         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-//     ],
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.(js|jsx)$/i,
-//                 loader: 'babel-loader',
-//             },
-//             {
-//                 test: /\.css$/i,
-//                 use: [stylesHandler,'css-loader'],
-//             },
-//             {
-//                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-//                 type: 'asset',
-//             },
-
-//             // Add your rules for custom modules here
-//             // Learn more about loaders from https://webpack.js.org/loaders/
-//         ],
-//     },
-// };
-
-// module.exports = () => {
-//     if (isProduction) {
-//         config.mode = 'production';
-        
-        
-//         config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-        
-//     } else {
-//         config.mode = 'development';
-//     }
-//     return config;
-// };
-
-
-// {
-//     resolve: {
-//       modules: [...],
-//       fallback: {
-//         "fs": false,
-//         "tls": false,
-//         "net": false,
-//         "path": false,
-//         "zlib": false,
-//         "http": false,
-//         "https": false,
-//         "stream": false,
-//         "crypto": false,
-//         "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
-//       } 
-//     },
-//     entry: [...],
-//     output: {...},
-//     module: {
-//       rules: [...]
-//     },
-//     plugins: [...],
-//     optimization: {
-//       minimizer: [...],
-//     },
-//     // node: {
-//     //   fs: 'empty',
-//     //   net: 'empty',
-//     //   tls: 'empty'
-//     // },
-//     }
 
 const path = require('path');
 
@@ -105,7 +9,8 @@ module.exports = {
   entry: './src/index.js', // Your entry file
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'public/bundle.js', // Output bundle
+    filename: 'bundle.js', // Output bundle
+    publicPath: '/dist/'
   },
   devServer: {
     static: {                               
@@ -113,6 +18,12 @@ module.exports = {
       watch: true
     }
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico'
+    })
+  ],
   module: {
     rules: [
       {
