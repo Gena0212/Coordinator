0265f50033a224983fd7ab4013d60901a57cb7da
Gena Jey <thipi1213@live.com>
2025-01-08 00:27:30 -0500

    Set up database and one knex migration

---
+++ capstone-back-end/migrations/20250108043532_create_user_post_tables.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable("user", function (table) {
+    table.increments('id').primary();
+    table.string('email').notNullable();
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTable('user')
+}
