727293b5aa0c54484e735edd6c11ad03ae738150
Gena Jey <thipi1213@live.com>
2025-01-07 22:13:47 -0500

    Made API call successfully to get events

--- capstone-back-end/index.js
+++ capstone-back-end/index.js
@@ -1,8 +1,8 @@
 // const { google } = require('googleapis');
 
-import { google } from 'googleapis';
+import { google } from "googleapis";
 import express from "express";
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,79 +10,97 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-const scopes = ['https://www.googleapis.com/auth/calendar'];
+const scopes = ["https://www.googleapis.com/auth/calendar"];
 
 // OAuth 2 configuration
-const oauth2Client = new google.auth.OAuth2
-(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URL
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
 );
-
 
 const calendar = google.calendar({
-    version: 'v3', 
-    auth: oauth2Client
+  version: "v3",
+  auth: oauth2Client,
 });
 
-app.get('/auth', (req, res) => {
-
-    const url = oauth2Client.generateAuthUrl
-    ({
-        access_type: 'offline',
-        scope: scopes
-    });
-    res.redirect(url);
-    }
-);
+app.get("/auth", (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+  });
+  res.redirect(url);
+});
 
 app.get("/auth/redirect", async (req, res) => {
+  const { tokens } = await oauth2Client.getToken(req.query.code);
+  oauth2Client.setCredentials(tokens);
+  res.send("Authentication successful! Please return to the console.");
+});
 
-    const {tokens} = await oauth2Client.getToken(req.query.code);
-    oauth2Client.setCredentials(tokens);
-    res.send('Authentication successful! Please return to the console.');
+app.get("/events", async (req, res) => {
+  try {
+    const result = await calendar.events.list({
+      calendarId: "primary",
+      timeMin: new Date().toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: "startTime",
+    });
+
+    const events = result.data.items;
+
+    
+    if (!events || events.length === 0) {
+        console.log("No upcoming events found.");
+        return;
+    } else {
+        console.log(events);
     }
 
-);
+    res.send({
+      status: 200,
+      message: "Events fetched successfully",
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 const event = {
-    summary: 'Tech Talk with Arindam',
-    location: 'Google Meet',
+  summary: "Tech Talk with Arindam",
+  location: "Google Meet",
 
-    description: "Demo event for Arindam's Blog Post.",
-    start: {
-        dateTime: "2024-03-14T19:30:00+05:30",
-        timeZone: 'Asia/Kolkata'
-    },
-    end: {
-        dateTime: "2024-03-14T20:30:00+05:30",
-        timeZone: 'Asia/Kolkata'
-    },
-    attendees: [
-        {email: 'gena.jey@gmail.com'},
-    ]
+  description: "Demo event for Arindam's Blog Post.",
+  start: {
+    dateTime: "2024-03-14T19:30:00+05:30",
+    timeZone: "Asia/Kolkata",
+  },
+  end: {
+    dateTime: "2024-03-14T20:30:00+05:30",
+    timeZone: "Asia/Kolkata",
+  },
+  attendees: [{ email: "gena.jey@gmail.com" }],
 };
 
-app.get('/create-event', async (req, res) => {
+app.get("/create-event", async (req, res) => {
+  try {
+    const result = await calendar.events.insert({
+      calendarId: "primary",
+      auth: oauth2Client,
+      resource: event,
+    });
 
-    try {
-        const result = await calendar.events.insert({
-                                calendarId: 'primary', 
-                                auth:oauth2Client,
-                                resource: event
-                        });
-
-        res.send({
-            status: 200,
-            message: 'Event created',
-        });
-    } catch (err) {
-        console.log(err);
-        res.send(err);
-    }
-    }
-);
+    res.send({
+      status: 200,
+      message: "Event created",
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
