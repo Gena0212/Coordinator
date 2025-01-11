dca765ea5af40c2ce0e9de101e046b0150a5890b
Gena Jey <thipi1213@live.com>
2025-01-11 15:43:20 -0500

    Created LoginForm component

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import LoginForm from '../../components/LoginForm/LoginForm';
 
 
 const LoginPage = () => {
   const [accessToken, setAccessToken] = useState(null);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-  });
+
 
   useEffect(() => {
     // Function to extract authorization code from URL query parameters
@@ -59,47 +54,6 @@ const LoginPage = () => {
     window.location.href = `https://accounts.google.com/o/oauth2/auth?client_id=${import.meta.env.VITE_APP_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_APP_REDIRECT_URI}&response_type=code&scope=${import.meta.env.VITE_APP_SCOPES}`;
   };
 
-  const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setErrorMessage("");
-
-    if (!formData.email || !formData.password) {
-      setErrorMessage("You must provide a username and a password");
-      return;
-    }
-
-    // Check the format of the email address via a regular expression
-    if (!emailRegex.test(formData.email)) {
-      setErrorMessage("The email address is not valid. Expected format: x@x.xx");
-      return;
-    }
-
-    try {
-      // To login, send a POST request to the `/users/login` endpoint
-      const { data } = await axios.post(
-        `${import.meta.env.VITE_API_BASE_URL}/users/login`,
-        {
-          email: formData.email,
-          password: formData.password,
-        }
-      );
-
-      // To ensure the frontend stays logged in, store the JWT in localStorage
-      localStorage.setItem("authToken", data.authToken);
-
-      setSuccess(true);
-      setTimeout(() => {
-        navigate("/profile");
-      }, 2000);
-    } catch (error) {
-      setErrorMessage(error.response.data.message);
-    }
-  };
-
   return (
     <div>
       <h1>Login</h1>
@@ -108,26 +62,7 @@ const LoginPage = () => {
           <p>Logged in successfully!</p>
         </div>
       ) : (
-        <>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="firstName">Username</label>
-            <input
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleInputChange}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleInputChange}
-            />
-          </form>
-          <button>Sign In</button>
-          {/* <button onClick={handleLogin}>Login with Google</button> */}
-        </>
+        <LoginForm/>
       )}
     </div>
   );
