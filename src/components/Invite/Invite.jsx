39afe607a3e304b6da745ece1c3ffff4adb8bca2
Gena Jey <thipi1213@live.com>
2025-01-16 21:59:51 -0500

    Add functionality to accept invites

--- src/components/Invite/Invite.jsx
+++ src/components/Invite/Invite.jsx
@@ -1,17 +1,39 @@
+import axios from "axios"
+
 function Invite({groupInfo}){
     const members = groupInfo.members
-    console.log(members)
+
+    const apiURL = import.meta.env.VITE_API_BASE_URL
+    const authToken = localStorage.getItem('authToken');
+
+    const acceptInvite = async () => {
+        try {
+            const response = await axios.put( `${apiURL}/groups/invites/${groupInfo.id}`, {accept_invite: 1}, 
+                {
+                    headers: {
+                      authorisation: `Bearer ${authToken}`,
+                    },
+                }
+            )
+            console.log(response.status)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return(
         <section>
             <h3>{groupInfo.name}</h3>
             {members.map((member) => {
                 return (
-                    <>
-                        <p>{member.firstName}</p>
-                    </>
+                    <div key={member.id}>
+                        <p>{member.firstName} {member.lastName}</p>
+                        <p>{member.email}</p>
+                    </div>
+                    
                 )
             })}
+            <button onClick={acceptInvite}>Accept Invite</button>
         </section>
     )
 }
