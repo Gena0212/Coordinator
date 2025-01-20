1fdbfc2d1afba1872bab81254960f9ee7430bbca
Gena Jey <thipi1213@live.com>
2025-01-19 22:45:34 -0500

    Fix small bug

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,18 +4,10 @@ import LoginForm from '../../components/LoginForm/LoginForm';
 import Header from '../../components/Header/Header';
 import './LoginPage.scss'
 import { Link } from 'react-router-dom';
-// import googleButton from '../../assets/images/btn_google_signin_dark_pressed_web.png'
 
 
 const LoginPage = ({formData, setFormData}) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const [formData, setFormData] = useState({
-  //   email: "",
-  //   password: "",
-  // });
-
-  // const apiURL = import.meta.env.VITE_API_BASE_URL
-
 
   useEffect(() => {
     // Function to extract authorization code from URL query parameters
@@ -23,37 +15,13 @@ const LoginPage = ({formData, setFormData}) => {
       const urlParams = new URLSearchParams(window.location.search);
       const code = urlParams.get('code');
 
-      // if (code) { 
-      //   // Exchange the authorization code for an access token
-      //   exchangeAuthorizationCodeForAccessToken(code);
-      // }
     };
 
     getAuthorizationCode();
   }, []);
 
-  // function navigate(url){
-  //   window.location.href = url;
-  // }
-  
-  // async function auth(){
-  //   try {
-  //     const response = await axios.post(`${apiURL}/request/${formData.email}`)
-  //     console.log(response.data);
-  //     navigate(response.data.url);
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-
   return (
     <div>
-      {/* {isLoggedIn ? ( */}
-        {/* // <button type="button" onClick={()=> auth()}>
-        //       <img src={googleButton} alt='google sign in'/>
-        //     </button> */}
-      {/* ) : ( */}
         <>
           <Header isLoggedIn={false}/>
           <section className='login'>
@@ -64,7 +32,6 @@ const LoginPage = ({formData, setFormData}) => {
             </Link>
           </section>
         </>
-      {/* )} */}
     </div>
   );
 };
